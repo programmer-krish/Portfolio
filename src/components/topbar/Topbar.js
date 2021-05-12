72d123f1ca56bc2c7738fd55cee504fc553670a6
programmer-krish <s18002156@ousl.lk>
2021-05-12 17:39:42 +0530

    Menu Added

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./topbar.scss";
 import { Person, Mail, GitHub, LinkedIn } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar active">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="https://github.com/programmer-krish" className="logo">
@@ -25,9 +25,8 @@ export default function Topbar() {
             <span>rkrishnamohan96@gmail.com</span>
           </div>
         </div>
-        <h1>Hello</h1>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
